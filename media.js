0e8ebeb5881f7156a69d0a5c9c9f547d52877b02
tom2a2hawk <wingdingworkshop@gmail.com>
2025-01-28 22:51:19 -0500

    starting the wiki

---
+++ media.js
@@ -0,0 +1,4 @@
+const bloodlust_mercy_wiki = document.getElementById('bloodlust_mercy_wiki');
+bloodlust_mercy_wiki.addEventListener('click', function() {
+  window.location.href = "bloodlust&mercy/bloodlust-mercy-wiki.html";
+});
